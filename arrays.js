7c41d88cbbe488f724a5a79607a8f224ad4408e8
Pablo Argueta <pabloargueta27@gmail.com>
2018-04-28 01:34:35 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,8 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+
+function addElementToBeginningOfArray([], element){
+  
+}
+function destructivelyAddElementToBeginningOfArray([], element) {
+  
+}
