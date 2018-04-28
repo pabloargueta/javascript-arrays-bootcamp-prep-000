23037782415e7b682690ccfc80ececb05db7b3ca
Pablo Argueta <pabloargueta27@gmail.com>
2018-04-28 01:45:34 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -9,3 +9,10 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
   array.unshift(element);
   return array;
 }
+
+function addElementToEndOfArray(){
+  
+}
+function destructivelyAddElementToEndOfArray() {
+  
+}
