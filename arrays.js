37ca2e111cad22d0c4ec0e82b0354703789de60f
Pablo Argueta <pabloargueta27@gmail.com>
2018-04-28 01:48:03 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -18,4 +18,6 @@ function destructivelyAddElementToEndOfArray(array, element) {
   array.push(element);
   return array;
 }
-function accessElementInArray
+function accessElementInArray(array, index) {
+  return array[element];
+}
