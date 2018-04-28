f7d134ad8be3d9137f80c047371a4699bf94b6cf
Pablo Argueta <pabloargueta27@gmail.com>
2018-04-28 02:03:36 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -23,6 +23,6 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
   return array;
 }
 function removeElementFromBeginnningOfArray(array) {
-  array.slice(1);
+  array = array.slice(1);
   return array;
 }
