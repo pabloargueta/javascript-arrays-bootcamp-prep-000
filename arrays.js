96e640f8893c71707958057a9e15e1f959420786
Pablo Argueta <pabloargueta27@gmail.com>
2018-04-28 01:41:56 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
 function addElementToBeginningOfArray(array, element) {
-  var array = array.unshift(element);
+  array.unshift(element);
   return array;
 }
 function destructivelyAddElementToBeginningOfArray(array, element) {
